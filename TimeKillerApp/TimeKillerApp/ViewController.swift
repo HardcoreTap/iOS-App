794585be6db953826098be6b64e4b22e3fe5782e
Sergey <s.dunaev@icloud.com>
2017-11-11 15:15:56 +0300

    Добавил работу таймера
    Добавил работу таймера, убрал mainButton. Добавил жест-рекогнайзер.

--- TimeKillerApp/TimeKillerApp/ViewController.swift
+++ TimeKillerApp/TimeKillerApp/ViewController.swift
@@ -12,8 +12,12 @@ import SCLAlertView
 class ViewController: UIViewController {
     
     var count: Int = 0
-    var seconds: Int = 0
-    var timer: Timer?
+    var seconds: Int = 0               // Счетчик секунд
+    var seconds10: Int = 0             // Счетчик десятых секунды
+    var interval10: Double = 0.1       // Текущий интервал десятой секунды
+    let deltaInterval10: Double = 0.01 // Дельта изменеия интервала секунды для ускорения
+    var timer = Timer()
+    var flPlaying: Bool = false // Флаг запуска игры
     var uid: String = ""
     let username = mainInstance.name
     
@@ -22,128 +26,151 @@ class ViewController: UIViewController {
     var rootRef = Database.database().reference()
     var scoreRef: DatabaseReference!
     
-
+    
     @IBOutlet weak var switchModeGame: UISwitch!
     @IBOutlet weak var hardcoreLabel: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var highScoreLabel: UILabel!
     @IBOutlet weak var startGameButton: UIButton!
-    @IBOutlet weak var mainTapButton: UIButton! //клик в игре - который самый главный
-
-
     
     
-    //MARK : viewDidLoad
+    //MARK: - viewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //скрываем все лишнее, и ждем нажатия кнопки "Начать игру"
-        self.scoreLabel.isHidden = true
-
+        scoreLabel.isHidden = true
+        
         //подгрузка рекорда из UserDefaults
         if UserDefaults.standard.value(forKey: "highscore") != nil {
             
             highScore = UserDefaults.standard.value(forKey: "highscore") as! Int
             
-            self.highScoreLabel.text = "Ваш рекорд: \(highScore)"
+            highScoreLabel.text = "Ваш рекорд: \(highScore)"
             
         }
         
         //Firebase
         scoreRef = rootRef.child("High Score")
         
+        // Регистрация рекогнайзера жестов
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap))
+        view.addGestureRecognizer(tapGR)
     }
     
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        startGameButton.isHidden = false
+    }
+    
+    @objc func didTap(tapGR: UITapGestureRecognizer) {
+        if flPlaying {
+            // Проверка точности попадания
+            if seconds10 == 0 {
+                // Плюс очко
+                count += 1
+                scoreLabel.text = "Очки: \(count)"
+                interval10 -= deltaInterval10
+            } else {
+                gameOver()
+            }
+        }
+    }
+    
+    func timerBlock(timer: Timer) {
+        
+        seconds10 += 1
+        if seconds10 == 10 {
+            seconds += 1
+            seconds10 = 0
+            
+            // Смена таймеров так сделано, чтобы не было притормаживание секундомера
+            let newTimer = Timer.scheduledTimer(withTimeInterval: interval10, repeats: true, block: timerBlock(timer:))
+            timer.invalidate()
+            self.timer = newTimer
+        }
+        updateTimerLabel()
+        
+        if seconds10 == 1 && seconds > count {
+            // Пропущено нажатие
+            gameOver()
+        }
+    }
     
     @IBAction func startGameButtonDidTapped(_ sender: Any) {
         
-        self.mainTapButton.isHidden = false
-        self.highScoreLabel.isHidden = false
-        self.scoreLabel.isHidden = false
+        highScoreLabel.isHidden = false
+        scoreLabel.isHidden = false
         
-        self.startGameButton.isHidden = true
-        self.hardcoreLabel.isHidden = true
-        self.switchModeGame.isHidden = true
+        startGameButton.isHidden = true
+        hardcoreLabel.isHidden = true
+        switchModeGame.isHidden = true
         
-        self.setupGame()
+        setupGame()
         
     }
     
-    
-    @IBAction func buttonPressed(_ sender: Any) {
-        count += 1
+    func setupGame() {
+        
+        count = 0
+        seconds = 0
+        seconds10 = 0
+        interval10 = 0.1
+        flPlaying = true
+        
+        updateTimerLabel()
         scoreLabel.text = "Очки: \(count)"
+        
+        timer = Timer.scheduledTimer(withTimeInterval: interval10, repeats: true, block: timerBlock(timer:))
+        
+    }
+    
+    func gameOver() {
+        timer.invalidate()
+        flPlaying = false
         
         //добавления нового рекорда
         if count > highScore {
             
             highScore = count
-            self.highScoreLabel.text = "Ваш рекорд: \(highScore)"
+            highScoreLabel.text = "Ваш рекорд: \(highScore)"
             
             UserDefaults.standard.set(highScore, forKey: "highscore")
             
         }
         
-    }
-    
-    
-    func setupGame() {
+        let itemRef = scoreRef.childByAutoId()
         
-        seconds = 5
-        count = 0
+        let scoreItem = [
+            "username": UserDefaults.standard.value(forKey: "userNAME") as! String,
+            "highscore": UserDefaults.standard.value(forKey: "highscore") as! Int
+            ] as [String : Any]
         
-        timerLabel.text = "\(seconds)"
-        scoreLabel.text = "Очки: \(count)"
+        //отправка данных в Firebase
+        itemRef.setValue(scoreItem)
         
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.subtractTime), userInfo: nil, repeats: true)
         
-    }
-    
-    @objc func subtractTime() {
-        seconds -= 1
-        timerLabel.text = "\(seconds)"
+        //MARK: SCLAlertView после окончания игры
+        let appearance = SCLAlertView.SCLAppearance(showCloseButton: false)
+        let alertView = SCLAlertView(appearance: appearance)
         
-        if seconds == 0 {
-            timer?.invalidate()
-            
-             let itemRef = self.scoreRef.childByAutoId()
-
-             let scoreItem = [
-                "username": UserDefaults.standard.value(forKey: "userNAME") as! String,
-                "highscore": UserDefaults.standard.value(forKey: "highscore") as! Int
-                ] as [String : Any]
-
-            //отправка данных в Firebase
-            itemRef.setValue(scoreItem)
-            
-            
-            //MARK: SCLAlertView после окончания игры
-            let appearance = SCLAlertView.SCLAppearance(showCloseButton: false)
-            let alertView = SCLAlertView(appearance: appearance)
-            
-            alertView.addButton("Начать заново") {
-                //рестарт игры
-                self.setupGame()
-            }
-            
-            alertView.addButton("Таблица лидеров") {
-                //переходим на страницу с лидербоард
-                self.tabBarController?.selectedIndex = 1
-            }
-            
-            alertView.showSuccess("Поздравляем!", subTitle: "Вы набрали \(count). очков")
-            
-            
-            
+        alertView.addButton("Начать заново") {
+            //рестарт игры
+            self.setupGame()
         }
         
+        alertView.addButton("Таблица лидеров") {
+            //переходим на страницу с лидербоард
+            self.tabBarController?.selectedIndex = 1
+        }
+        
+        alertView.showSuccess("Поздравляем!", subTitle: "Вы набрали \(count). очков")
     }
-
     
+    func updateTimerLabel() {
+        timerLabel.text = String(format: "00:%02d:%d", seconds, seconds10)
+    }
     
 }
 
