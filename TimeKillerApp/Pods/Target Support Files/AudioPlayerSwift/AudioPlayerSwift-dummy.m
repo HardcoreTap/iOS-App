a3d4d35c4340be43577f456113fc03a1b15f926c
Bogdan Bystritskiy <q8061@ya.ru>
2017-11-15 04:06:27 +0300

    Успешно переименовал проект

--- TimeKillerApp/Pods/Target Support Files/AudioPlayerSwift/AudioPlayerSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AudioPlayerSwift : NSObject
-@end
-@implementation PodsDummy_AudioPlayerSwift
-@end
--- TimeKillerApp/Pods/Target Support Files/Pods-TimeKillerApp/Pods-TimeKillerApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TimeKillerApp : NSObject
-@end
-@implementation PodsDummy_Pods_TimeKillerApp
-@end
